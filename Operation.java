c89222f75d4e4330e6f0f4136ee27d3ef19ff916
vyshnav suresh <93926562+VyshnavSuresh@users.noreply.github.com>
2022-03-22 11:58:58 +0530

    22-3-2022

---
+++ Operation.java
@@ -0,0 +1,7 @@
+class Operation{
+    public static void main(String[]a){
+		int c=10,b=20;
+		System.out.println("OPERATIONS \n 1."+c+"+"+b+"="+(c+b)+"\n 2."+c+"-"+b+"="+(c-b)+"\n 3."+c+"/"+b+"="+(c/b)+"\n 4."+c+"*"+b+"="+(c*b));
+	
+    }
+}
